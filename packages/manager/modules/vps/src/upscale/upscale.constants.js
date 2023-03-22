46fd4d9bc7291f5421859af175f34361da42a6b6
Rafik Adiche <rafik.adiche.ext@corp.ovh.com>
2023-03-22 08:34:53 +0100

    feat(vps.upscale): add agreements links
    ref: MANAGER-7321
    Signed-off-by: Rafik Adiche <rafik.adiche.ext@corp.ovh.com>

--- packages/manager/modules/vps/src/upscale/upscale.constants.js
+++ packages/manager/modules/vps/src/upscale/upscale.constants.js
@@ -5,6 +5,23 @@ export const PRICING_MODES = {
   UPFRONT: 'upfront',
 };
 
+export const AGREEMENTS = {
+  EU: {
+    termsOfService:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/2f391d2-contrat_genServices-FR-14.1.pdf',
+    privacyPolicy: 'https://www.ovhcloud.com/fr/personal-data-protection/',
+  },
+  CA: {
+    termsOfService:
+      'https://storage.gra.cloud.ovh.net/v1/AUTH_325716a587c64897acbef9a4a4726e38/contracts/2f391d2-contrat_genServices-FR-14.1.pdf',
+    privacyPolicy: 'https://www.ovhcloud.com/fr/personal-data-protection/',
+  },
+  US: {
+    termsOfService: 'https://us.ovhcloud.com/legal/terms-of-service',
+    privacyPolicy: 'https://us.ovhcloud.com/legal/privacy-policy',
+  },
+};
+
 export const RANGES = {
   BESTVALUE: 'bv',
   COMFORT: 'Comfort',
@@ -14,4 +31,4 @@ export const RANGES = {
   VALUE: 'Value',
 };
 
-export default { PRICING_MODES, RANGES };
+export default { PRICING_MODES, RANGES, AGREEMENTS };
